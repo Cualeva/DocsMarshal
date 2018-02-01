9349144c6eefc941082097471d85f88c3abc5bcc
IlCave <luca.cavedaghi@cualeva.com>
2018-02-01 20:25:03 +0100

    Sistemati Bug

--- DOCSMarshal.Interfaces/Managers/Profile/IProfileQueryManager.cs
+++ DOCSMarshal.Interfaces/Managers/Profile/IProfileQueryManager.cs
@@ -5,7 +5,6 @@ namespace DocsMarshal.Interfaces.Managers.Profile
 {
     public interface IProfileQueryManager: IDisposable
     {
-        DocsMarshal.Entities.ProfileSearchResult Execute(DocsMarshal.Entities.ProfileSearch query);
         Task<DocsMarshal.Entities.ProfileSearchResult> ExecuteAsync(DocsMarshal.Entities.ProfileSearch query);
     }
 }
--- DOCSMarshal.Interfaces/Managers/Profile/IProfileSearchManager.cs
+++ DOCSMarshal.Interfaces/Managers/Profile/IProfileSearchManager.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace DocsMarshal.Interfaces.Managers.Profile
 {
     public interface IProfileSearchManager: IDisposable
     {
-        Entities.ProfileSearchResult ById(Guid Id);
-        Entities.ProfileSearchResult ByDynAssId(Guid dynAssId, Guid objectId);
-        Entities.ProfileSearchResult ByDynAssId(Guid dynAssId, List<Guid> objectIds);
-        Entities.ProfileSearchResult ByDynAssExternalId(string dynAssExternalId, Guid objectId);
-        Entities.ProfileSearchResult ByDynAssExternalId(string dynAssExternalId, List<Guid> objectIds);
+        Task<Entities.ProfileSearchResult> ById(Guid Id);
+        Task<Entities.ProfileSearchResult> ByDynAssId(Guid dynAssId, Guid objectId);
+        Task<Entities.ProfileSearchResult> ByDynAssId(Guid dynAssId, List<Guid> objectIds);
+        Task<Entities.ProfileSearchResult> ByDynAssExternalId(string dynAssExternalId, Guid objectId);
+        Task<Entities.ProfileSearchResult> ByDynAssExternalId(string dynAssExternalId, List<Guid> objectIds);
         IProfileQueryManager Query { get; }
     }
 }
