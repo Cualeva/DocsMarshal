28d258c2dce65d7c5bc8b3db59c95f6bf85e4e07
IlCave <luca.cavedaghi@cualeva.com>
2019-07-18 16:16:48 +0200

    .

--- DOCSMarshal.Orchestrator/Managers/UrlsManager.cs
+++ DOCSMarshal.Orchestrator/Managers/UrlsManager.cs
@@ -18,6 +18,19 @@ namespace DocsMarshal.Orchestrator.Managers
             Orchestrator = null;
         }
 
+        public string DocumentModels_CreateAndDownloadDocumentModelFromObjectId(Guid objectId, string documentModelExternalId)
+        {
+            if (Orchestrator != null)
+                return DocumentModels_CreateAndDownloadDocumentModelFromObjectId(objectId, documentModelExternalId, Orchestrator.SessionId);
+            else
+                return DocumentModels_CreateAndDownloadDocumentModelFromObjectId(objectId, documentModelExternalId, string.Empty);
+        }
+
+        public string DocumentModels_CreateAndDownloadDocumentModelFromObjectId(Guid objectId, string documentModelExternalId, string sessionId)
+        {
+            return string.Format("{0}/DocumentModel/CreateAndDownloadDocumentModelFromObjectId?objectId={1}&documentModelExternalId={2}&sessionID={3}", Orchestrator.DocsMarshalUrl, objectId, documentModelExternalId, sessionId);
+        }
+
         public string Profile_GetDocumentByFieldExternalId(Guid objectId, string fieldExternalId)
         {
             return Profile_GetDocumentByFieldExternalId(objectId, fieldExternalId, Orchestrator.SessionId);
