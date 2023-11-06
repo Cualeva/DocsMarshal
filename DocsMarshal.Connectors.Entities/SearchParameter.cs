54ecdf24f51c30d89a6d634a50bd05d01143c767
CualeMarke <luigi.marchesi@cualeva.com>
2023-11-06 09:37:06 +0100

    Add DynAssPathExternalIds to search conditions

--- DocsMarshal.Connectors.Entities/SearchParameter.cs
+++ DocsMarshal.Connectors.Entities/SearchParameter.cs
@@ -4,5 +4,6 @@ namespace DocsMarshal.Connectors.Entities
     public abstract class SearchParameter
     {
         public string FieldExternalId { get; set; }
+        public string DynAssExternalIdsPath { get; set; }
     }
 }
